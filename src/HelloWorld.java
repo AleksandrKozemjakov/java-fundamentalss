94cf7eee5bae4bf9a326029dc7241c4bf584d041
AleksandrKozemjakov <kozemjakov@gmail.com>
2022-09-14 19:23:11 +0300

    HelloWorld Created

--- src/HelloWorld.java
+++ src/HelloWorld.java
@@ -1,2 +1,10 @@
+/**
+ * The first Hello world program
+ *
+ * @author Aleksandr Kozemjakov
+ */
 public class HelloWorld {
-}
+     public static void main(String[] args) {
+         System.out.println("Hello,World!");
+     }
+ }
