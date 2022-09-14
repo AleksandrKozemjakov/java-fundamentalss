81627e6c15fa9af575922213deaa0e9834c1cc62
AleksandrKozemjakov <kozemjakov@gmail.com>
2022-09-14 20:05:36 +0300

    Added datatypes

---
+++ src/DataType.java
@@ -0,0 +1,32 @@
+/**
+ * Examples of Primitive datatype
+ *
+ * @Author Aleksandr Kozemjakov
+ */
+public class DataType {
+    int a = 1;
+    byte byteNumber =120;
+    long longNumber = 5454545465L;
+    short shortNumber = -32000;
+    float decimalNumber = 2.45f;
+    double doubleDecimalNumber = 3.456d;
+
+    //Character
+    char alpha = 'F';
+
+    //True/False
+    boolean hadLunch = true; // had lunch?
+    boolean isHoliday = false;
+
+    /* Multi-line
+    comment
+     */
+
+    int i; 0
+
+    float x; 0.00f
+
+    char animal; //''
+
+    boolean isClassTomorrow; //false
+}
