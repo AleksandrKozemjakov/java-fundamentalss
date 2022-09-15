a394af6e18667eb289af4a720049f5db0a6c2917
AleksandrKozemjakov <kozemjakov@gmail.com>
2022-09-15 20:58:42 +0300

    Added datatype example

---
+++ src/Homework.java
@@ -0,0 +1,20 @@
+public class Homework {
+    public static void main(String[] args) {
+        int i = 9;
+        int a = 18;
+
+        int result = // your expression here;
+        /*
+        1.
+        2.
+        3.
+        4.
+        5.
+        6.
+        7.
+
+         */
+        sout result
+
+    }
+}
