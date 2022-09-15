6dc8b2950d1f1b3f1fc53cd29db1fa412ff6b8a9
AleksandrKozemjakov <kozemjakov@gmail.com>
2022-09-15 20:45:09 +0300

    Added datatype example

--- src/HelloWorld.java
+++ src/HelloWorld.java
@@ -4,8 +4,8 @@
  * @author Aleksandr Kozemjakov
  */
 public class HelloWorld {
-     public static void main(String[] args) {
-         System.out.println("Hello,World!");
+     public static void main(String[] args) { //psvm
+         System.out.println("Hello,World!");  //sout
 
          System.out.println(DataType.Food);
      }
